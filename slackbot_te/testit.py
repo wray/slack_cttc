695f2d00f52028893b59f3aad8d550e8f676b05e
wray <wray@wrayesian.com>
2016-11-19 16:51:25 -0500

    fixed test :)

--- slackbot_te/testit.py
+++ slackbot_te/testit.py
@@ -21,8 +21,8 @@ class TestSlackBotFunctions(unittest.TestCase):
             wray.slacklib.COMMAND1)) > 1)
 
     def test_topic_map(self):
-        self.assertTrue(wray.slacklib.handle(command('topic:python')).find('python') >= 0)
-        self.assertTrue(wray.slacklib.handle(command('topics')).find('python') >= 0)
+        self.assertTrue(wray.slacklib.handle_command('topic:python').find('python') >= 0)
+        self.assertTrue(wray.slacklib.handle_command('topics').find('python') >= 0)
 
 
 
